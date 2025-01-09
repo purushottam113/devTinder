d0f4196d8c8a797acc196bafc0f543c228763f90
purushottam113 <shreepad113@gmail.com>
2025-01-09 18:46:15 +0530

    API level data sentiziation

--- src/app.js
+++ src/app.js
@@ -26,8 +26,99 @@ app.use(
     express.json()
 )
 
-app.post("/signup", async (req,res)=>{
+// Update user data in database
+app.patch("/user/:userID",async (req,res)=>{    
+    const data = await req.body;
+    const userID = await req.params?.userID;
     
+    try {
+        const Allowed_Updates = ["userID", "firstName", "lastName", "age", "gender", "skills"]
+
+        const isAllowedUpdates = Object.keys(data).every((k)=>
+                                    Allowed_Updates.includes(k)
+                                );
+        if(!isAllowedUpdates){
+            throw new Error("Enter proper keys")
+        };
+
+        if(data.skills.length > 10){
+            throw new Error("Skills upto 10")
+        }
+
+        await User.findByIdAndUpdate(userID, data, {
+            runValidators : true
+        });
+
+        res.send("Data Update Succesfully")
+
+    } catch (error) {
+        res.status(400).send("someting went wrong: " + error.message);
+    };
+});
+
+// Update user data in database
+app.put("/user",async (req,res)=>{
+    try {
+        const data = await req.body;
+        const userID = await req.body.userID;
+
+        await User.findByIdAndUpdate(userID, data);
+        res.send("Data Update Succesfully")
+
+    } catch (error) {
+        res.status(400).send("someting went wrong");
+    }
+})
+
+// Delete user
+app.delete("/user", async (req,res)=>{
+    try {
+        const deletedID = await User.findByIdAndDelete(req.body.userID)
+        res.send("User Delete Successfully")
+    } catch (error) {
+        res.status(400).send("Someting went  wrong");
+    }
+
+})
+
+//get data by email
+app.get("/user", async (req,res)=>{
+    try{
+        const userDetails = await User.find(req.body);
+
+        if(userDetails.length === 0){
+            res.send("User not found")
+        }
+        else{
+            res.send(userDetails)
+        }
+    }
+    catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+//get all data 
+
+app.get("/feed", async (req,res)=>{
+    try{
+        const users = await User.find({});
+
+        if(users.length === 0){
+            res.send("User not found")
+        }
+        else{
+            res.send(users)
+        }
+    }
+    catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// signup route
+app.post("/signup", async (req,res)=>{
+
     const user = new User(req.body)
 
     try{
--- src/modules/user.js
+++ src/modules/user.js
@@ -2,24 +2,52 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
+        minLength  : 4,
+        maxLength : 20,
+        trim : true,
     },
     lastName : {
-        type : String
+        type : String,
+        trim : true,
     } ,
     emailID : {
-        type : String
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
     } ,
     password : {
-        type : String
+        type : String,
+        required : true,
     } ,
     age : {
-        type : Number
+        type : Number,
+        min : 18,
     } ,
     gender : {
-        type : String
-    } 
-});
+        type : String,
+        trim : true,
+        validate(value){
+            if(!["male", "female", "others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        },      
+    } ,
+    about : {
+        type : String,
+        default : "Hi... I am devTinder user...."
+    } ,
+    skills : {
+        type : [String],
+    } ,
+} ,
+    {
+        timestamps: true, 
+    }
+);
 
 const User = mongoose.model("User", userSchema);
 
